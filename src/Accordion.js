8644d80d98572d7f934a87deca21741c40671368
Michael Romero <michael.romero09@gmail.com>
2019-05-22 11:47:40 -0700

    Accordion component fully functional

---
+++ src/Accordion.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Accordion.css';
+
+class Accordian extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      openSection: null
+    }
+  }
+
+  renderSection(section, i) {
+    let content = '';
+    if (i === this.state.openSection) {
+      content = <p>{section.content}</p>
+    }
+    return (
+      <li key={i}>
+        <button onClick={this.handleClick} id={i}>{section.title}</button>
+        {content}
+      </li>
+    )
+  }
+
+  handleClick = (e) => {
+    let newIndex = parseInt(e.target.id)
+    if (this.state.openSection === newIndex) {
+      this.setState({
+        openSection : null
+      })
+    } else {
+      this.setState({
+        openSection : newIndex
+      })
+    }
+  }
+
+  render() {
+    return (
+      <ul className="Accordion">
+        {this.props.sections.map((section, i) => this.renderSection(section, i))}
+      </ul>
+    )
+  }
+}
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+Accordian.defaultProps = ({sections});
+
+export default Accordian;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RouletteGun from './RouletteGun';
+import Accordion from './Accordion';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<Accordion />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
